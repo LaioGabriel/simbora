a9acb423417f8e574fbaf79e250251468519a781
Your Name <you@example.com>
2023-07-31 12:44:53 -0300

    mensagem

---
+++ script.js
@@ -0,0 +1,10 @@
+function togglemode(){
+    const html = document.documentElement
+   // if( html.classList.contains("light")){
+     //         html.classList.remove("light") 
+   //         } else{
+  //  html.classList.add ("light")
+   //         }
+   html.classList.toggle("light");
+
+}
